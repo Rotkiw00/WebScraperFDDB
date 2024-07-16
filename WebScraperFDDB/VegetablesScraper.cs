d6ea4cb8f57e83be09ec22a84cf7773a166a52c7
Wiktor Kalaga <wkalaga73@gmail.com>
2024-07-16 20:37:18 +0200

    Initial commit. Created solution

---
+++ WebScraperFDDB/VegetablesScraper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraperFDDB
+{
+    internal class VegetablesScraper
+    {
+        private const string BaseUri = "https://fddb.info/db/en/groups/vegetables/index.html";
+    }
+}
